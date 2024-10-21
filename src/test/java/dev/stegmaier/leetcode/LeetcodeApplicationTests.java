009ae42bac430ce6f3be34bf2b58b6ae0bf77f3f
John Stegmaier <john@stegmaier.dev>
2024-10-20 22:17:41 -0400

    Add unit test with leetcode example cases

---
+++ src/test/java/dev/stegmaier/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.stegmaier.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
