18d20308e1342962a8731055c54a4aba192c2ae5
John Stegmaier <jupdown@protonmail.com>
2025-06-22 17:53:36 -0400

    Implement handling for inapropriate input

--- src/main/java/dev/stegmaier/leetcode/services/LeetCodeQuestions/Q3085MinimumDeletionService.java
+++ src/main/java/dev/stegmaier/leetcode/services/LeetCodeQuestions/Q3085MinimumDeletionService.java
@@ -11,6 +11,21 @@ public class Q3085MinimumDeletionService {
   private static final Logger log = getLogger(Q3085MinimumDeletionService.class);
 
   public int getMinimumDeletions(String expression, int kValue) {
+    checkExpressionMeetsRequirements(expression);
+
     return 0;
+  }
+
+  private void checkExpressionMeetsRequirements(String expression) {
+    if(null == expression) {
+      throw new NullPointerException("expression is somehow null");
+    }
+    if (expression.isEmpty()) {
+      throw new IllegalArgumentException("expression length less than 1");
+    }
+    if(expression.length() > 100000) {
+      throw new IllegalArgumentException("expression length exceeds 10⁵");
+    }
+
   }
 }
--- src/test/java/dev/stegmaier/leetcode/services/LeetCodeQuestions/Q3085MinimumDeletionServiceTest.java
+++ src/test/java/dev/stegmaier/leetcode/services/LeetCodeQuestions/Q3085MinimumDeletionServiceTest.java
@@ -1,5 +1,6 @@
 package dev.stegmaier.leetcode.services.LeetCodeQuestions;
 
+import static dev.stegmaier.leetcode.constants.Q3085Constants.STRING_WITH_100001_CHARACTERS;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -37,5 +38,48 @@ class Q3085MinimumDeletionServiceTest {
     int result = q3085MinimumDeletionService.getMinimumDeletions(inputString, inputKValue);
 
     assertEquals(1, result);
+  }
+/*
+@Test
+void exceptionTesting() {
+    MyException thrown = assertThrows(
+           MyException.class,
+           () -> myObject.doThing(),
+           "Expected doThing() to throw, but it didn't"
+    );
+
+    assertTrue(thrown.getMessage().contains("Stuff"));
+}
+ */
+  @Test
+  void shouldNotHandleStringOver100000CharactersLong() {
+    String inputString = STRING_WITH_100001_CHARACTERS;
+    int inputKValue = 0;
+
+    IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> q3085MinimumDeletionService.getMinimumDeletions(inputString, inputKValue), "Expected q3085MinimumDeletionService to throw an exception when a string with more than 100,000 characters was provided");
+
+    assertTrue(illegalArgumentException.getMessage().equals("expression length exceeds 10⁵"), "q3085MinimumDeletionService did not throw the correct exception message when a string with more than 100,000 characters was provided");
+  }
+
+  @Test
+  void shouldNotHandleStringLessThanOneCharacterInLength() {
+    String inputString = "";
+    int inputKValue = 0;
+
+    IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> q3085MinimumDeletionService.getMinimumDeletions(inputString, inputKValue), "Expected q3085MinimumDeletionService to throw an exception when a string smaller than 1 character was provided");
+
+    assertTrue(illegalArgumentException.getMessage().equals("expression length less than 1"), "q3085MinimumDeletionService did not throw the correct exception message when a string smaller than 1 character was provided");
+
+  }
+
+  @Test
+  void shouldNotHandleNullString() {
+    String inputString = null;
+    int inputKValue = 0;
+
+    NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> q3085MinimumDeletionService.getMinimumDeletions(inputString, inputKValue), "Expected q3085MinimumDeletionService to throw an exception when a null string was provided");
+
+    assertTrue(nullPointerException.getMessage().equals("expression is somehow null"), "q3085MinimumDeletionService did not throw the correct exception message when a null string was provided");
+
   }
 }
